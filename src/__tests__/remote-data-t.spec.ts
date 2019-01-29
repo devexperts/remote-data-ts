3824e91e456406e5a6310c3b23b8cd5ba8d8142e
Yuriy Bogomolov <cronus.cpp@gmail.com>
2019-01-29 17:49:20 +0300

    Rename `fromEither` in test

--- src/__tests__/remote-data-t.spec.ts
+++ src/__tests__/remote-data-t.spec.ts
@@ -9,7 +9,7 @@ describe('RemoteDataT', () => {
 		const of = remoteDataT.getRemoteDataT(task).of;
 		const f = (n: number) => of(n * 2);
 		const x = of(1);
-		const y = remoteDataT.fromEither(task)<string, number>(remoteData.failure('foo'));
+		const y = remoteDataT.fromRemoteData(task)<string, number>(remoteData.failure('foo'));
 
 		const [e1, e2] = await Promise.all([chain(f, x).run(), chain(f, y).run()]);
 

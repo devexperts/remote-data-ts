962891cd75914875a3926ccc6117a40604ed16bd
raveclassic <raveclassic@gmail.com>
2019-08-20 16:19:21 +0100

    feature: classless

--- src/__tests__/remote-data-io.spec.ts
+++ src/__tests__/remote-data-io.spec.ts
@@ -1,39 +1,39 @@
 import { createRemoteDataFromJSON } from '../remote-data-io';
-import * as t from 'io-ts';
 import { initial, pending, failure, success, progress } from '../remote-data';
 import { right } from 'fp-ts/lib/Either';
 import { none, some } from 'fp-ts/lib/Option';
+import { number, string } from 'io-ts';
 
 describe('RemoteDataFromJSONType', () => {
 	it('createRemoteDataFromJSON', () => {
-		const T = createRemoteDataFromJSON(t.string, t.number);
-		expect(T.decode({ type: 'Failure', error: 'error' })).toEqual(right(failure('error')));
-		expect(T.decode({ type: 'Initial' })).toEqual(right(initial));
-		expect(T.decode({ type: 'Pending', progress: null })).toEqual(right(pending));
-		expect(T.decode({ type: 'Pending', progress: { loaded: 2, total: null } })).toEqual(
+		const codec = createRemoteDataFromJSON(string, number);
+		expect(codec.decode({ _tag: 'RemoteFailure', error: 'error' })).toEqual(right(failure('error')));
+		expect(codec.decode({ _tag: 'RemoteInitial' })).toEqual(right(initial));
+		expect(codec.decode({ _tag: 'RemotePending', progress: null })).toEqual(right(pending));
+		expect(codec.decode({ _tag: 'RemotePending', progress: { loaded: 2, total: null } })).toEqual(
 			right(progress({ loaded: 2, total: none })),
 		);
-		expect(T.decode({ type: 'Pending', progress: { loaded: 2, total: 5 } })).toEqual(
+		expect(codec.decode({ _tag: 'RemotePending', progress: { loaded: 2, total: 5 } })).toEqual(
 			right(progress({ loaded: 2, total: some(5) })),
 		);
-		expect(T.decode({ type: 'Success', value: 42 })).toEqual(right(success(42)));
-		expect(T.encode(failure('error'))).toEqual({ type: 'Failure', error: 'error' });
-		expect(T.encode(initial)).toEqual({ type: 'Initial' });
-		expect(T.encode(pending)).toEqual({ type: 'Pending', progress: null });
-		expect(T.encode(progress({ loaded: 2, total: none }))).toEqual({
-			type: 'Pending',
+		expect(codec.decode({ _tag: 'RemoteSuccess', value: 42 })).toEqual(right(success(42)));
+		expect(codec.encode(failure('error'))).toEqual({ _tag: 'RemoteFailure', error: 'error' });
+		expect(codec.encode(initial)).toEqual({ _tag: 'RemoteInitial' });
+		expect(codec.encode(pending)).toEqual({ _tag: 'RemotePending', progress: null });
+		expect(codec.encode(progress({ loaded: 2, total: none }))).toEqual({
+			_tag: 'RemotePending',
 			progress: { loaded: 2, total: null },
 		});
-		expect(T.encode(progress({ loaded: 2, total: some(5) }))).toEqual({
-			type: 'Pending',
+		expect(codec.encode(progress({ loaded: 2, total: some(5) }))).toEqual({
+			_tag: 'RemotePending',
 			progress: { loaded: 2, total: 5 },
 		});
-		expect(T.encode(success(42))).toEqual({ type: 'Success', value: 42 });
-		expect(T.is(failure('error'))).toBe(true);
-		expect(T.is(initial)).toBe(true);
-		expect(T.is(pending)).toBe(true);
-		expect(T.is(success(42))).toBe(true);
-		expect(T.is(failure(1))).toBe(false);
-		expect(T.is(success('invalid'))).toBe(false);
+		expect(codec.encode(success(42))).toEqual({ _tag: 'RemoteSuccess', value: 42 });
+		expect(codec.is(failure('error'))).toBe(true);
+		expect(codec.is(initial)).toBe(true);
+		expect(codec.is(pending)).toBe(true);
+		expect(codec.is(success(42))).toBe(true);
+		expect(codec.is(failure(1))).toBe(false);
+		expect(codec.is(success('invalid'))).toBe(false);
 	});
 });
--- src/remote-data-io.ts
+++ src/remote-data-io.ts
@@ -1,136 +1,44 @@
-import * as t from 'io-ts';
-import { fold as foldO } from 'fp-ts/lib/Option';
-import { isLeft } from 'fp-ts/lib/Either';
-import { pipe } from 'fp-ts/lib/pipeable';
-
-import {
-	failure,
-	initial,
-	pending,
-	RemoteData,
-	RemoteFailure,
-	RemoteInitial,
-	RemotePending,
-	RemoteSuccess,
-	success,
-	progress,
-} from './remote-data';
+import { RemoteData, RemoteFailure, RemoteInitial, RemotePending, RemoteProgress, RemoteSuccess } from './remote-data';
 import { optionFromNullable } from 'io-ts-types/lib/optionFromNullable';
+import { literal, number, type, Type, union } from 'io-ts';
 
-export interface JSONFailure<L> {
-	type: 'Failure';
-	error: L;
-}
-
-export interface JSONInitial {
-	type: 'Initial';
-}
-
-export interface JSONProgress {
+export type JSONRemoteProgress = {
 	loaded: number;
 	total: number | null;
-}
+};
 
-export interface JSONPending {
-	type: 'Pending';
-	progress: JSONProgress | null;
-}
+export type JSONRemotePending = {
+	_tag: 'RemotePending';
+	progress: JSONRemoteProgress | null;
+};
 
-export interface JSONSuccess<A> {
-	type: 'Success';
-	value: A;
-}
+export type JSONRemoteData<E, A> = RemoteFailure<E> | RemoteInitial | JSONRemotePending | RemoteSuccess<A>;
 
-export type JSONRemoteData<L, A> = JSONFailure<L> | JSONInitial | JSONPending | JSONSuccess<A>;
+const RemoteInitialCodec: Type<RemoteInitial> = type({
+	_tag: literal('RemoteInitial'),
+});
 
-export class RemoteDataFromJSONType<L extends t.Any, R extends t.Any, A = any, O = A, I = t.mixed> extends t.Type<
-	A,
-	O,
-	I
-> {
-	readonly _tag: 'RemoteDataFromJSONType' = 'RemoteDataFromJSONType';
-	constructor(
-		name: string,
-		is: RemoteDataFromJSONType<L, R, A, O, I>['is'],
-		validate: RemoteDataFromJSONType<L, R, A, O, I>['validate'],
-		serialize: RemoteDataFromJSONType<L, R, A, O, I>['encode'],
-		readonly left: L,
-		readonly right: R,
-	) {
-		super(name, is, validate, serialize);
-	}
-}
+const RemoteProgressCodec: Type<RemoteProgress, JSONRemoteProgress> = type({
+	loaded: number,
+	total: optionFromNullable(number),
+});
 
-export function createRemoteDataFromJSON<
-	L extends t.Type<AL, OL>,
-	R extends t.Type<AR, OR>,
-	AL = t.TypeOf<L>,
-	OL = t.OutputOf<L>,
-	AR = t.TypeOf<R>,
-	OR = t.OutputOf<R>
->(
-	leftType: L,
-	rightType: R,
-	name: string = `RemoteData<${leftType.name}, ${rightType.name}>`,
-): RemoteDataFromJSONType<L, R, RemoteData<AL, AR>, JSONRemoteData<OL, OR>, t.mixed> {
-	const JSONProgress = optionFromNullable(
-		t.type({
-			loaded: t.number,
-			total: optionFromNullable(t.number),
-		}),
-	);
-	const JSONFailure = t.type({
-		type: t.literal('Failure'),
-		error: leftType,
+const RemotePendingCodec: Type<RemotePending, JSONRemotePending> = type({
+	_tag: literal('RemotePending'),
+	progress: optionFromNullable(RemoteProgressCodec),
+});
+
+export function createRemoteDataFromJSON<LA, LO, RA, RO>(
+	leftCodec: Type<LA, LO>,
+	rightCodec: Type<RA, RO>,
+): Type<RemoteData<LA, RA>, JSONRemoteData<LO, RO>> {
+	const RemoteFailureCodec = type({
+		_tag: literal('RemoteFailure'),
+		error: leftCodec,
 	});
-	const JSONInitial = t.type({
-		type: t.literal('Initial'),
+	const RemoteSuccessCodec = type({
+		_tag: literal('RemoteSuccess'),
+		value: rightCodec,
 	});
-	const JSONPending = t.type({
-		type: t.literal('Pending'),
-		progress: JSONProgress,
-	});
-	const JSONSuccess = t.type({
-		type: t.literal('Success'),
-		value: rightType,
-	});
-	const JSONRemoteData = t.taggedUnion('type', [JSONFailure, JSONInitial, JSONPending, JSONSuccess]);
-	return new RemoteDataFromJSONType(
-		name,
-		(m): m is RemoteData<AL, AR> =>
-			m instanceof RemoteInitial ||
-			m instanceof RemotePending ||
-			(m instanceof RemoteFailure && leftType.is(m.error)) ||
-			(m instanceof RemoteSuccess && rightType.is(m.value)),
-		(m, c) => {
-			const validation = JSONRemoteData.validate(m, c);
-			if (isLeft(validation)) {
-				return validation as any;
-			} else {
-				const e = validation.right;
-				switch (e.type) {
-					case 'Failure':
-						return t.success(failure(e.error));
-					case 'Initial':
-						return t.success(initial);
-					case 'Pending':
-						return pipe(
-							e.progress,
-							foldO(() => t.success(pending), p => t.success(progress(p))),
-						);
-					case 'Success':
-						return t.success(success(e.value));
-				}
-			}
-		},
-		a =>
-			a.foldL<JSONRemoteData<OL, OR>>(
-				() => ({ type: 'Initial' }),
-				progress => ({ type: 'Pending', progress: JSONProgress.encode(progress) }),
-				l => ({ type: 'Failure', error: leftType.encode(l) }),
-				a => ({ type: 'Success', value: rightType.encode(a) }),
-			),
-		leftType,
-		rightType,
-	);
+	return union([RemoteInitialCodec, RemotePendingCodec, RemoteFailureCodec, RemoteSuccessCodec], '_tag');
 }
